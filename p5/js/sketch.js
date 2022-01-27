a053140e06a2842e838f1bcf4558bfc6b616e18d
Emily O'Connell <emilykoconnell@gmail.com>
2022-01-27 13:43:14 -0600

    hello p5

--- p5/js/sketch.js
+++ p5/js/sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  background(2,155,152);
 }
 
 function draw() {
-  // put drawing code here
+  stroke(30,200,100);
+  strokeWeight(1);
+  line(20,40,300,200);
+  
+  stroke(100,50,80);
+  fill(0,255,0);
+  strokeWeight(5);
+  ellipse(windowWidth/2,windowHeight/2,50,70);
+
+  noStroke();
+  fill(255,0,0);
+  rect(mouseX,mouseY,300,50);
+  
+
 }
