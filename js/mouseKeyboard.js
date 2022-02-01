0f9583c572a3522466a419c0d52c1c7c2db28e1a
Emily O'Connell <emilykoconnell@gmail.com>
2022-02-01 13:42:16 -0600

    mouse and keyboard input practice

---
+++ js/mouseKeyboard.js
@@ -0,0 +1,28 @@
+let ellipseX = 500;
+
+function setup(){
+
+    createCanvas(windowWidth,windowHeight)
+    background(180,55,0);
+}
+
+function draw(){
+    if(mouseIsPressed == true){
+        background(40,100,100)
+    }
+    else{
+        background(60,55,32)
+    }
+
+    background(180,55,200);
+    ellipse(ellipseX,mouseY, 100)
+
+    if(mouseX > windowWidth/2){
+        fill(30,200,150)
+    }
+    else{
+        fill(200,40,100)
+    }
+
+    
+}
