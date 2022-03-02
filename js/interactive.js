7cfb0751f3f97df646aa774ba70503bb6841aa67
Emily O'Connell <emilykoconnell@gmail.com>
2022-03-02 13:42:47 -0600

    working buttons

--- js/interactive.js
+++ js/interactive.js
@@ -7,61 +7,284 @@ var selection
 function preload(){
     feelingHype = loadImage('images/star-eyes.png')
     feelingSad = loadImage('images/broken-heart.png')
-}
-
-// randomly select a happy song, then return it
-function chooseHappy () {
-    var happySongs=["0VE4kBnHJUgtMf0dy6DRmW","1BxfuPKGuaTgP7aM0Bbdwr", "6rZVy6FIG7lSJQMFXHo12z", "7A2cNLRT0YJc1yjxHlKihs", "0yN4fNzmVnmgC0dsOoi9Wh"]
-    var happyId=random(happySongs)
-    
-    return happyId
+    feelingSadButHype = loadImage('images/sad-cowboy.png')
+    feelingInLove = loadImage('images/heart.png')
+    tellMeAStory = loadImage('images/open-book.png')
+    goatsOnly = loadImage('images/goat.png')
 }
 
 
 function setup() {
-    myCanvas = createCanvas(windowWidth, windowHeight-300);
+    myCanvas = createCanvas(windowWidth, windowHeight);
     myCanvas.parent('myContainer')
 
-    //Create, style and resize clickables.
+    /* HYPE TAYLOR */
+
     hypeButton = new Clickable()
     hypeButton.image = feelingHype
-    hypeButton.locate(20, 20)
+    hypeButton.fitImage = true
+    hypeButton.locate(windowWidth/8, 350)
 
-    //This function is ran when the clickable is hovered but not pressed.
+   
     hypeButton.onHover = function () {
-        this.color = "#AAAAFF"
-        //this.image = " "
+        this.stroke = "#AAAAFF"
         this.textColor = "#000000";
         this.text = "Hype!";
+        this.image = ""
+
         }
-    //This function is ran when the clickable is NOT hovered.
+   
     hypeButton.onOutside = function () {
-        this.color = "#EEEEEE";
-        this.image - feelingHype
+        this.color = "#e6fadc";
+        this.stroke = "#e6fadc";
+        this.image = feelingHype
         this.text = " ";
         this.textColor = "#000000";
         }
-    //This function is ran when the clickable is pressed.
+   
     hypeButton.onPress = function () {
-        openSpotify(chooseHappy());
+        openSpotify(chooseHype());
     }
 
-    //This funcion is ran when the cursor was pressed and then
-    //rleased inside the clickable. If it was pressed inside and
-    //then released outside this won't work.
+
     hypeButton.onRelease = function () {
         this.color = "#EEEEEE";
-        this.text = "normal ";
+        this.textColor = "#000000";
+    }
+    /* SAD TAYLOR */
+
+
+    sadButton = new Clickable()
+    sadButton.image = feelingSad
+    sadButton.fitImage = true
+    sadButton.locate(2*windowWidth/8, 350)
+
+    
+    sadButton.onHover = function () {
+        this.stroke = "#AAAAFF"
+        this.textColor = "#000000";
+        this.text = "Sad!";
+        this.image = ""
+    }
+    
+    sadButton.onOutside = function () {
+        this.color = "#e6fadc";
+        this.stroke = "#e6fadc";
+        this.image = feelingSad
+        this.text = " ";
+        this.textColor = "#000000";
+    }
+    
+    sadButton.onPress = function () {
+        openSpotify(chooseSad());
+    }
+
+
+    sadButton.onRelease = function () {
+        this.color = "#EEEEEE";
+        this.textColor = "#000000";
+    }
+
+
+    /* SAD BUT HYPE TAYLOR */
+
+
+    sadButHypeButton = new Clickable()
+    sadButHypeButton.image = feelingSadButHype
+    sadButHypeButton.fitImage = true
+    sadButHypeButton.locate(3*windowWidth/8, 350)
+
+   
+    sadButHypeButton.onHover = function () {
+        this.stroke = "#AAAAFF"
+        this.textColor = "#000000";
+        this.text = "Sad but Hype!";
+        this.image = ""
+
+        }
+   
+    sadButHypeButton.onOutside = function () {
+        this.color = "#e6fadc";
+        this.stroke = "#e6fadc";
+        this.image = feelingSadButHype
+        this.text = " ";
+        this.textColor = "#000000";
+        }
+   
+    sadButHypeButton.onPress = function () {
+        openSpotify(chooseSadButHype());
+    }
+
+
+    sadButHypeButton.onRelease = function () {
+        this.color = "#EEEEEE";
+        this.textColor = "#000000";
+    }
+
+    /* IN LOVE TAYLOR */
+
+
+    inLoveButton = new Clickable()
+    inLoveButton.image = feelingSadButHype
+    inLoveButton.fitImage = true
+    inLoveButton.locate(4*windowWidth/8, 350)
+
+   
+    inLoveButton.onHover = function () {
+        this.stroke = "#AAAAFF"
+        this.textColor = "#000000";
+        this.text = "In Love!";
+        this.image = ""
+
+        }
+   
+    inLoveButton.onOutside = function () {
+        this.color = "#e6fadc";
+        this.stroke = "#e6fadc";
+        this.image = feelingInLove
+        this.text = " ";
+        this.textColor = "#000000";
+        }
+   
+    inLoveButton.onPress = function () {
+        openSpotify(chooseLove());
+    }
+
+
+    inLoveButton.onRelease = function () {
+        this.color = "#EEEEEE";
+        this.textColor = "#000000";
+    }
+
+    /* TELL ME A STORY TAYLOR */
+
+
+    storyButton = new Clickable()
+    storyButton.image = tellMeAStory
+    storyButton.fitImage = true
+    storyButton.locate(5*windowWidth/8, 350)
+
+   
+    storyButton.onHover = function () {
+        this.stroke = "#AAAAFF"
+        this.textColor = "#000000";
+        this.text = "Tell me a Story!";
+        this.image = ""
+
+        }
+   
+    storyButton.onOutside = function () {
+        this.color = "#e6fadc";
+        this.stroke = "#e6fadc";
+        this.image = tellMeAStory
+        this.text = " ";
+        this.textColor = "#000000";
+        }
+   
+    storyButton.onPress = function () {
+        openSpotify(chooseStory());
+    }
+
+
+    storyButton.onRelease = function () {
+        this.color = "#EEEEEE";
+        this.textColor = "#000000";
+    }
+    /* GOAT Taylor */
+
+    goatButton = new Clickable()
+    goatButton.image = goatsOnly
+    goatButton.fitImage = true
+    goatButton.locate(6*windowWidth/8, 350)
+
+   
+    goatButton.onHover = function () {
+        this.stroke = "#AAAAFF"
+        this.textColor = "#000000";
+        this.text = "Greatest Hits!";
+        this.image = ""
+
+        }
+   
+    goatButton.onOutside = function () {
+        this.color = "#e6fadc";
+        this.stroke = "#e6fadc";
+        this.image = goatsOnly
+        this.text = " ";
+        this.textColor = "#000000";
+        }
+   
+    goatButton.onPress = function () {
+        openSpotify(chooseGoat());
+    }
+
+
+    goatButton.onRelease = function () {
+        this.color = "#EEEEEE";
         this.textColor = "#000000";
     }
 }
 function draw() {
     background(230,250,220);
-    hypeButton.draw();
+    hypeButton.draw()
+    sadButton.draw()
+    sadButHypeButton.draw()
+    inLoveButton.draw()
+    storyButton.draw()
+    goatButton.draw()
+
+}
+
+// randomly select a hype song, then return it
+function chooseHype () {
+    var hypeSongs=["0VE4kBnHJUgtMf0dy6DRmW","1BxfuPKGuaTgP7aM0Bbdwr", "6rZVy6FIG7lSJQMFXHo12z", "7A2cNLRT0YJc1yjxHlKihs", "0yN4fNzmVnmgC0dsOoi9Wh"]
+    var hypeId=random(hypeSongs)
+    
+    return hypeId
+}
+
+// randomly select a sad song, then return it
+function chooseSad () {
+    var sadSongs = ["0VE4kBnHJUgtMf0dy6DRmW","1BxfuPKGuaTgP7aM0Bbdwr", "6rZVy6FIG7lSJQMFXHo12z", "7A2cNLRT0YJc1yjxHlKihs", "0yN4fNzmVnmgC0dsOoi9Wh"]
+    var sadId=random(sadSongs)
+    
+    return sadId
+}
+
+// randomly select a sad but hype song, then return it
+function chooseSadButHype () {
+    var sadButHypeSongs=["0VE4kBnHJUgtMf0dy6DRmW","1BxfuPKGuaTgP7aM0Bbdwr", "6rZVy6FIG7lSJQMFXHo12z", "7A2cNLRT0YJc1yjxHlKihs", "0yN4fNzmVnmgC0dsOoi9Wh"]
+    var sadButHypeId=random(sadButHypeSongs)
+    
+    return sadButHypeId
+}
+
+// randomly select a love song, then return it
+function chooseLove () {
+    var loveSongs=["0VE4kBnHJUgtMf0dy6DRmW","1BxfuPKGuaTgP7aM0Bbdwr", "6rZVy6FIG7lSJQMFXHo12z", "7A2cNLRT0YJc1yjxHlKihs", "0yN4fNzmVnmgC0dsOoi9Wh"]
+    var loveId=random(loveSongs)
+    
+    return loveId
+}
+
+// randomly select a storytelling song, then return it
+function chooseStory () {
+    var storySongs=["0VE4kBnHJUgtMf0dy6DRmW","1BxfuPKGuaTgP7aM0Bbdwr", "6rZVy6FIG7lSJQMFXHo12z", "7A2cNLRT0YJc1yjxHlKihs", "0yN4fNzmVnmgC0dsOoi9Wh"]
+    var storyId=random(storySongs)
+    
+    return storyId
+}
+
+// randomly select a goat song, then return it
+function chooseGoat () {
+    var goatSongs=["0VE4kBnHJUgtMf0dy6DRmW","1BxfuPKGuaTgP7aM0Bbdwr", "6rZVy6FIG7lSJQMFXHo12z", "7A2cNLRT0YJc1yjxHlKihs", "0yN4fNzmVnmgC0dsOoi9Wh"]
+    var goatId=random(goatSongs)
+    
+    return goatId
 }
 
 function openSpotify(songId){
-    // if already open, close it
+    // if spotify is already open, close it
     if (spotifyOpen==1){
         closeSpotify()
     }
@@ -75,15 +298,11 @@ function openSpotify(songId){
         iframe.src=loc;
         iframe.allow="encrypted-media";
         iframe.width=myCanvas.width;
-        iframe.height=myCanvas.height;
-        //iframe.position
-        
+        iframe.height = 300
+        myCanvas.resize(windowWidth, windowHeight-300)
         myContainer.appendChild(iframe);
         spotifyOpen=1;
         closeSpotifyButton.mousePressed(closeSpotify);
-    }
-    if (spotifyOpen==1) {
-        //closeSpotify()
     }
 
 }
@@ -92,55 +311,6 @@ function closeSpotify(){
     iframe.remove();
     closeSpotifyButton.remove();
     spotifyOpen=0;
+    myCanvas.resize(windowWidth, windowHeight)
     redraw();
 }
-
-
-//function draw() {
-//noLoop();
-//background(230,250,220);
-
-//}
-// //original
-// //openSpotifyButton=createButton('Listen on Spotify');
-// //openSpotifyButton.position(width/4,height/2);
-// //openSpotifyButton.mousePressed(openSpotify);
-// //feelingHype.position(19, 19);
-// //image(feelingHype, 19, 19, 50,50)
-// //feelingHype.mousePressed(changeBG);
-
-// //hype
-// //feelingHype=createButton('Hype');
-// //feelingHype.position(width/7,height/2);
-// image(feelingHype, 19, 19, 50, 50)
-// feelingHype.mousePressed(openSpotify(chooseHappy()))
-
-// //sad
-// //feelingSad=createButton('Sad');
-// //feelingSad.position(2*width/7,height/2);
-// image(feelingSad, 40, 40, 50, 50)
-// feelingSad.mousePressed(openSpotify(chooseHappy()))
-
-// //feelingSad.mousePressed(openSpotify);
-
-// //sad but hype
-// feelingSadButHype = createButton('Sad but Hype');
-// feelingSadButHype.position(3 * width / 7, height / 2);
-// feelingSadButHype.mousePressed(openSpotify);
-
-// //in love
-// feelingInLove = createButton('In Love');
-// feelingInLove.position(4 * width / 7, height / 2);
-// feelingInLove.mousePressed(openSpotify);
-
-// //storytelling
-// storytellingTS = createButton('Storytelling');
-// storytellingTS.position(5 * width / 7, height / 2);
-// storytellingTS.mousePressed(openSpotify);
-
-// //S-tier
-// goatTS = createButton('GOAT');
-// goatTS.position(6 * width / 7, height / 2);
-// goatTS.mousePressed(openSpotify);
-
-//}
